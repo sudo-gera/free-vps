4efc5cdedcee0a31c4ff86905c2f3a438b9d2d94
sudo-gera <gera.tatarinov.999@gmail.com>
2026-02-07 02:32:57 +0300

    *interesting commit message*

--- executed_from_ci.sh
+++ executed_from_ci.sh
@@ -8,7 +8,7 @@ find ~/.ssh -type f -exec chmod 600 {} \;
 
 env > ~/env.txt
 
-apt install -y tmate
+sudo apt install -y tmate
 
 ( set -x ; export USERNAME='gera07a02-vpnjantit.com'     && export HOSTNAME='usa3.vpnjantit.com'     && export PASSWORD='1234567890'     && while sleep 1 ; do ( echo 'ssh -oPasswordAuthentication=yes -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -vvv -l "${USERNAME}" "${HOSTNAME}" -N -R 127.0.0.1:0:127.0.0.1:22 ; exit' ; for q in $(seq 1 160) ; do sleep 0.1 ; if grep password: /tmp/vpnjantit-"$(id -u)".txt >/dev/null ; then printf '%s\n' "$PASSWORD" ; exit ; fi ; done ; for q in $(seq 1 160) ; do sleep 0.1 ; printf $'\x03\x04' ; done ; ) | if script --version ; then script -f /tmp/vpnjantit-"$(id -u)".txt ; else script -F /tmp/vpnjantit-"$(id -u)".txt bash ; fi ; done )
 
